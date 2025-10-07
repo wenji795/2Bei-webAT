684579199278ca63b4b55ba009c3278f7b87cd40
Wenji Shen <swjoanna795@gmail.com>
2025-10-07 16:25:30 +1300

    2.2浏览器驱动可化名

--- 2.2浏览器初始化.py
+++ 2.2浏览器初始化.py
@@ -1,5 +1,5 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 
 #初始化浏览器时有两个关键参数：options 和 service
@@ -9,7 +9,7 @@ options = webdriver.ChromeOptions()
 # options.add_argument("--disable-gpu")  # 示例：禁用 GPU
 
 # 配置驱动服务
-service = Service(
+service = ChromeService(
     executable_path=ChromeDriverManager().install(),
     log_output="1.log"   # 将日志输出到文件
 )
