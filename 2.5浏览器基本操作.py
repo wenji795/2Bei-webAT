81ca1a0f6ce1c875d6c02022ac1e938e20361119
Wenji Shen <swjoanna795@gmail.com>
2025-10-09 12:21:04 +1300

    2.5浏览器基本操作

--- 2.5浏览器基本操作.py
+++ 2.5浏览器基本操作.py
@@ -16,19 +16,30 @@ driver.set_window_position(300,300)
 driver.minimize_window()
 driver.maximize_window()
 driver.find_element(By.LINK_TEXT, "访问 新浪 网站").click()
-# time.sleep(2)
-# driver.back()
 time.sleep(2)
-# driver.find_element(By.XPATH,"//a[.='新浪']").click()
-# time.sleep(2)
+#多窗口 切换到指定窗口
+handles= driver.window_handles
+print("handles:", handles)
+print(driver.current_window_handle, driver.title)
+driver.switch_to.window(handles[1])
+print("handles:", handles)
+print(driver.current_window_handle, driver.title)
+driver.close()
+time.sleep(2)
+
+
+
+driver.find_element(By.XPATH,"//a[.='新浪']").click()
+time.sleep(2)
 driver.back()
+time.sleep(2)
 driver.forward()
-# driver.back()
+driver.back()
 driver.refresh()
 print(driver.title)
 print(driver.current_url)
 
-driver.close()
+
 
 
 
