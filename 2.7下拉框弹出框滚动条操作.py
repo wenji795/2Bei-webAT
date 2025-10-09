52da9d4272bfa064d20720eeb14580463112478e
Wenji Shen <swjoanna795@gmail.com>
2025-10-09 14:02:44 +1300

    2.7

---
+++ 2.7下拉框弹出框滚动条操作.py
@@ -0,0 +1,45 @@
+#导包
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
+
+#创建一个浏览器对象
+driver = webdriver.Chrome(service=Service("/Users/swj_795/Desktop/chromedriver-mac-arm64/chromedriver"))
+
+#打开某个HTML页面
+driver.get("file:///Users/swj_795/Desktop/%E5%8C%97%E6%A2%A6%E6%B5%8B/pagetest/%E6%B3%A8%E5%86%8CA.html")
+
+#核心操作
+#下拉框用Select类来处理是原生的HTML<select>标签
+# driver.find_element(By.XPATH,"//option[.='广州']").click()
+# select = Select(driver.find_element(By.XPATH, "//select[@id='selectA']"))
+# select.select_by_index(1)
+# select.select_by_value("sz")
+# select.select_by_visible_text("广州")
+
+#弹出框
+#操作alert弹窗
+# driver.find_element(By.ID, "alerta").click()
+# alert = driver.switch_to.alert
+# print(alert.text)
+# alert.accept()
+# #操作confirm弹窗
+# driver.find_element(By.ID, "confirma").click()
+# confirm = driver.switch_to.alert
+# print(confirm.text)
+# confirm.accept()
+
+#操作prompt
+# driver.find_element(By.ID, "prompta").click()
+# prompt = driver.switch_to.alert
+# prompt.send_keys("hello")
+# time.sleep(2)
+# print(prompt.text)
+# prompt.dismiss()
+
+#操作滚动条
+
+#等待
+time.sleep(5)
