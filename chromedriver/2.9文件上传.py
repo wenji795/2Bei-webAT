783262fb8b71ced179571bfc8ee95a1700cacad8
Wenji Shen <swjoanna795@gmail.com>
2025-10-10 15:31:28 +1300

    2.9文件上传

---
+++ chromedriver/2.9文件上传.py
@@ -0,0 +1,25 @@
+#导包
+import os.path
+import time
+from selenium import webdriver
+from selenium.webdriver import Keys, ActionChains
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
+
+#创建一个浏览器对象
+driver = webdriver.Chrome(service=Service("/Users/swj_795/Desktop/chromedriver-mac-arm64/chromedriver"))
+
+#打开某个HTML页面
+# driver.get("file:///Users/swj_795/Desktop/%E5%8C%97%E6%A2%A6%E6%B5%8B/pagetest/%E6%B3%A8%E5%86%8CA.html")
+
+#核心操作
+el = driver.find_element(By.NAME, "upfilea")
+#获取绝对路径，相对路径会报错
+file_path = os.path.abspath("chromedrive.exe")#os是python内置的
+el.send_keys(file_path)
+
+# el.click()
+
+#等待
+time.sleep(3)
